51f79119d5d90be52d9f1ac7530b0e397454a78f
aandrey <andrey.akimov@abgroup.tech>
2026-07-17 15:50:54 +0300

    feat: add words

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,3 +1,15 @@
+/*
+Game flow
+0) isOn -> true
+
+0) if team 1 is current, reselect letter
+1) show stat screen with the current coming-up team
+2) show start button
+3) for the time + last word, show words
+4) show stat, write to store, change current team
+5) -> 1
+*/
+
 export const Game = () => {
     return <div>Game</div>
 }
