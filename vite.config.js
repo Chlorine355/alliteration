4b8979e29990d95287883deb08406b5e5db039b1
aandrey <andrey.akimov@abgroup.tech>
2026-07-17 12:04:08 +0300

    feat: init app

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // или 'prompt'
+      manifest: {
+        name: 'Alliteration',
+        short_name: 'Alliteration',
+        description: 'PWA дял игры в объясненеи слов используя только слова на одну букву',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'src/assets/hero.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/screenshots/screenshot1.png',
+            sizes: '1280x720',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+        ],
+      },
+      // Опционально: конфигурация Workbox для кастомного кэширования
+    }),
+  ],
+});
