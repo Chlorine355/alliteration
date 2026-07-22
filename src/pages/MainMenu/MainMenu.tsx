57b9eb307d6322856c406cca3380052de7c0a779
aandrey <andrey.akimov@abgroup.tech>
2026-07-22 13:05:00 +0300

    feat: add warning modal

--- src/pages/MainMenu/MainMenu.tsx
+++ src/pages/MainMenu/MainMenu.tsx
@@ -1,20 +1,48 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './MainMenu.module.scss'
 import { $game } from '../../store/store'
 import { useUnit } from 'effector-react';
 import { setDefaultGameEv } from '../../store/actions';
+import { useState } from 'react';
 
 export const MainMenuPage = () => {
     const game = useUnit($game);
+    const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const startNewGame = () => {
+        setDefaultGameEv();
+        navigate('/teams')
+    }
     
+    const handleNewGameClick = () => {
+        if (game.isOn) {
+            setIsModalOpen(true);
+        } else {
+            startNewGame();
+        }
+    }
+
     return (
-    <div className={styles.container}>
-        <h1>Аллитерация</h1>
-        <div className={styles.buttons}>
-            <Link to={'/game'}><button className={'wide blue'} disabled={!game?.isOn}>Продолжить</button></Link>
-            <Link to={'/teams'} onClick={() => {setDefaultGameEv()}}><button className={'wide blue'}>Новая игра</button></Link>
-            <Link to={'/rules'}><button className={'wide blue'}>Правила</button></Link>
+        <div className={styles.container}>
+            <dialog open={isModalOpen} className={styles.modal}>
+                <div>Начать новую игру?</div>
+                <div>Прогресс сохранённой игры будет потерян.</div>
+                <div className={styles.modal_buttons}>
+                    <button onClick={() => setIsModalOpen(false)}>
+                        Отмена
+                    </button>
+                    <button onClick={startNewGame} className='blue'>
+                        Начать
+                    </button>
+                </div>
+            </dialog>
+            <h1>Аллитерация</h1>
+            <div className={styles.buttons}>
+                <Link to={'/game'}><button className={'wide blue'} disabled={!game?.isOn}>Продолжить</button></Link>
+                <button className={'wide blue'} onClick={handleNewGameClick}>Новая игра</button>
+                <Link to={'/rules'}><button className={'wide blue'}>Правила</button></Link>
+            </div>
         </div>
-    </div>
     )
 }
